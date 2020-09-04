89d360a494f601880fba5840d871781c82df0bb4
Yonn Alona <yalona@il-i5.msk.21-school.ru>
2020-09-04 15:07:01 +0300

    first printf

---
+++ include/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yalona <yalona@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/04 14:44:32 by yalona            #+#    #+#             */
+/*   Updated: 2020/09/04 15:06:29 by yalona           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <ctype.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct s_print
+{
+  
+} t_print;
+
+
+#endif
